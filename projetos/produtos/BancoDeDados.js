9116e15040b2ff2797429fa906e8db172f10b85f
lucimara <lucinha.lsd06@gmail.com>
2025-04-14 21:59:56 -0300

    aula produto 14/4

--- projetos/produtos/BancoDeDados.js
+++ projetos/produtos/BancoDeDados.js
@@ -1,24 +1,49 @@
+import { Produto } from "./Produto.js"
+
+
 export class BancoDeDados {
-    //CREATE
-     static salvar(produto) {
-        localStorage.setItem(produto.getId(), JSON.stringify(produto));
+    // CREATE
+    static salvar(produto) {
+        localStorage.setItem(produto.id, JSON.stringify({
+            id: produto.id,
+            nome: produto.nome,
+            preco: produto.preco,
+            peso: produto.peso,
+            validade: produto.validade
+        }));
     }
-    }
-    //READ
+
+    // READ
     buscarTodos() {
+     /// cria um array de produtos vazio
+     const produtos = [];
 
-
+     // percorre o localStorage buscando todos os dados (JSON)
+     for(let i = 0; i < localStorage.length; i++) {
+        const chave = localStorage.key(i);
+        if(!isNaN(parseInt(chave))) {
+            const dados = JSON.parse(localStorage.getItem(chave));
+            let produto = new Produto(dados.nome, dados.preco, dados.peso, dados.validade);
+            produtos.push(produtos); // insere os produtos no array;
+        }
     }
+
+     // retornar esse array ordenado
+     return produtos.sort((a,b) => a.id - b.id);     
+ }
+    
 
     buscarPorId(id) {
 
     }
-    //UPDATE
+
+    // UPDATE
     atualizar(produtoAtualizado) {
 
     }
 
     //DELETE
     excluir(id) {
-        
+
     }
+}
--- projetos/produtos/produtos.js
+++ projetos/produtos/produtos.js
@@ -1,34 +1,16 @@
-export class Produto {
-    static proximoId = 1;
-  id;
-  nome;
-  preco;
-  peso;
-  validade;
+import { BancoDeDados } from "./BancoDeDados.js";
 
-    constructor(nome, preco, peso, validade) {
-        this.id = Produto.proximoId++;
-        this.nome = nome;
-        this.preco = preco;
-        this.peso = peso;
-        this.validade = validade;
-    }
+const ul = document.getElementById("listaProdutos");
+const produtos = BancoDeDados.buscarTodos();
 
-    toString() {
-        let mensagem = `Produto ${this.nome} ID: ${this.id}`;
-        if(this.estaProximoDaValidade()) {
-            mensagem += " [ATENÇÃO: Próximo do Vencimento!]"
-        }
-        return mensagem;
-    }
+if(produtos.length === 0) {
+    ul.innerHTML = '<li>Nenhum produto cadastrado!</li>'
+} else {
+    produtos.forEach(p => {
+        const li = document.createElement("li"); 
+        
+        li.textContent = p.toString();
 
-    getId() {
-        return this.id;
-    }
-
-    estaProximoDaValidade() {
-        const hoje = new Date();
-        const diff = (this.validade - hoje) / (1000 * 60 * 60 * 24);
-        return diff < 10;
-    }
+        ul.appendChild(li);
+    });
 }
